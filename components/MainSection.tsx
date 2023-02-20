9dec33a1247fbf4c73a787a016435a8b3398fc9f
Geeko24 <kingnorway17@gmail.com>
2023-02-20 21:17:03 +0800

    update function

--- components/MainSection.tsx
+++ components/MainSection.tsx
@@ -2,12 +2,13 @@ import { useState, useEffect } from "react"
 import Modal from '@/components/Modal'
 import { v4 as uuidv4 } from 'uuid'
 
-
 export default function MainSection() {
+
+  
 
     const [closeModal, setCloseModal] = useState(false)
     const [storeInfo, setStoreInfo] = useState([] as any)
-    const [valueOnInput, setValueOnInpput] = useState({ 
+    const [valueOnInput, setValueOnInpput] = useState({
         //store here the data from input
         id: '',
         name: '',
@@ -15,6 +16,7 @@ export default function MainSection() {
         email: '',
         gender: ''
     })
+
     //ADD
     const handleFnameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         //use destructuring and get the data
@@ -22,14 +24,18 @@ export default function MainSection() {
         setValueOnInpput((prev) => {
             return (
                 //use ..prev for acessing the old data and combined [name]: to access the key name pass the destructured variables to make it dynamic
-                {...prev, [name]: value }
+                { ...prev, [name]: value }
             )
         })
+        
     }
+
+
 
     const addInfo = () => {
         //then pass the data here
         const infoObject = {
+            //or  uuidv4() for id
             id: storeInfo.length === 0 ? storeInfo.length + 1 : storeInfo.length + 1,
             name: valueOnInput.name,
             age: valueOnInput.age,
@@ -39,13 +45,32 @@ export default function MainSection() {
         setStoreInfo([...storeInfo, infoObject])
     }
 
-//DELETE
+    
+    //DELETE
     const deleteData = (id: any) => {
         //it will return the data that is not equal to id. if you have 3 list the you clicked the id#2 the it will return only id[1 nand 3]
         const deleteData = storeInfo.filter((info: any) => {
             return info.id !== id
         })
         setStoreInfo(deleteData)
+    }
+
+    //UPDATE
+    //it will store all data from child on update functions
+    const [update, setUpdate] = useState('')
+    const callback = (payload: any) => {
+        setUpdate(payload)
+    }
+
+    const updated = (id: any) => {
+        setStoreInfo(storeInfo.map((info: any) => {
+            if (info.id === id) {
+                    return {...info, name: update}
+            }
+            else {
+                return info
+            }
+            }))
     }
 
     return (
@@ -57,6 +82,7 @@ export default function MainSection() {
                         type="text"
                         className="py-1 px-2 border-2 rounded-md"
                         name="name"
+                        placeholder="John Doe"
                         onChange={handleFnameChange}
                     />
                 </div>
@@ -65,6 +91,7 @@ export default function MainSection() {
                     <input
                         type="text"
                         className="py-1 px-2 border-2 rounded-md"
+                        placeholder="56"
                         name="age"
                         onChange={handleFnameChange}
                     />
@@ -75,6 +102,7 @@ export default function MainSection() {
                     <input
                         type="email"
                         className="py-1 px-2 border-2 rounded-md"
+                        placeholder="johndoe@doe.com"
                         name="email"
                         onChange={handleFnameChange}
                     />
@@ -84,6 +112,7 @@ export default function MainSection() {
                     <input
                         type="text"
                         className="py-1 px-2 border-2 rounded-md"
+                        placeholder="male"
                         name="gender"
                         onChange={handleFnameChange}
                     />
@@ -91,7 +120,7 @@ export default function MainSection() {
                 <button className="w-36 btn-add" onClick={addInfo}>Add</button>
             </div>
 
-            <Modal hidden={closeModal} close={() => setCloseModal(false)} />
+            <Modal hidden={closeModal} close={() => setCloseModal(false)} update={ callback } />
             <table className="table-auto w-full">
                 <thead>
                     <tr className="text-center bg-green-400 h-12">
@@ -103,26 +132,27 @@ export default function MainSection() {
                         <th></th>
                         <th></th>
                         <th></th>
+                        <th></th>
                     </tr>
                 </thead>
-
-                {storeInfo.map((info: any) => {
-                    return (
-                        <>
-                            <tbody className="mt-2">
-                                <tr className="text-center border">
+                <tbody className="mt-2">
+                    {storeInfo.map((info: any) => {
+                        return (
+                                <tr className="text-center border" key={uuidv4()}>
                                     <td className=" py-4"> {info.id}</td>
                                     <td>{info.name}</td>
                                     <td>{info.age}</td>
                                     <td>{info.email}</td>
                                     <td>{info.gender}</td>
                                     <td><button className="btn-details">Details</button></td>
-                                    <td><button className="btn-update" onClick={() => setCloseModal(true)}>Update</button></td>
-                                    <td ><button className="btn-delete" onClick={()=> deleteData(info.id)}>Delete</button></td>
+                                    <td><button className="btn-update" onClick={() => setCloseModal(true)}>Edit</button></td>
+                                <td><button className="btn-delete" onClick={() => deleteData(info.id)}>Delete</button></td>
+                                <td><button className="btn-update" onClick={() => updated(info.id)}>Update</button></td>
+                                
                                 </tr>
-                            </tbody>
-                        </>)
-                })}
+                            )
+                    })}
+                </tbody>
 
             </table>
 
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,28 +1,62 @@
-export default function Modal(props: { hidden: any, close: any }) {
+import {useState} from 'react'
+export default function Modal(props: {update: any, hidden: any, close: any}) {
+
+const [update, setUpdate] = useState('')
+
+    const getvalue = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUpdate(e.target.value)
+    }
+
+    //two functions on onclick event
+    const functionsOnConfirmBtn = () => {
+        dataValue();
+        confirm();
+    }
+    const dataValue = () => {
+        props.update(update)
+    }
+    const confirm = () => {
+        props.close()
+    }
+
+
     return (
         <div className={props.hidden ? `visible flex absolute items-center w-full inset-0 bg-black/30` : `hidden`}>
             <div className="bg-white/70 h-[300px] w-4/12 mx-auto flex flex-col text-center justify-center rounded-md drop-shadow-lg backdrop-blur-sm">
                 <div className="p-2 grid grid-cols-1/2 items-center">
                     <label className="text-md font-semibold">Fullname</label>
-                    <input type="text" className="py-1 px-2 border-2 rounded-md" />
+                    <input
+                        type="text"
+                        className="py-1 px-2 border-2 rounded-md"
+                        onChange={getvalue}
+                    />
                 </div>
                 <div className="p-2 grid grid-cols-1/2">
                     <label className="text-md font-semibold">Age</label>
-                    <input type="text" className="py-1 px-2 border-2 rounded-md" />
+                    <input
+                        type="text"
+                        className="py-1 px-2 border-2 rounded-md"
+                    />
                 </div>
 
                 <div className="p-2 grid grid-cols-1/2">
                     <label className="text-md font-semibold">Email</label>
-                    <input type="email" className="py-1 px-2 border-2 rounded-md" />
+                    <input
+                        type="email"
+                        className="py-1 px-2 border-2 rounded-md"
+                    />
                 </div>
                 <div className="p-2 grid grid-cols-1/2">
                     <label className="text-md font-semibold">Gender</label>
-                    <input type="text" className="py-1 px-2 border-2 rounded-md" />
+                    <input
+                        type="text"
+                        className="py-1 px-2 border-2 rounded-md"
+                    />
                 </div>
 
                 <div className="space-x-10 flex justify-end mx-10 my-2">
                     <button className="btn-delete" onClick={props.close}>Close</button>
-                    <button className="btn-update">Update</button>
+                    <button className="btn-update" onClick={functionsOnConfirmBtn}>Confirm</button>
                 </div>
 
             </div>
